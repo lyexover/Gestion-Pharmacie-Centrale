0312a5950f1ff0c25e61cf695c70a5dbc184e3ec
lyes <lyesindel4@gmail.com>
2025-02-03 10:41:44 +0100

    modification interface auth

--- gestion-pharmacie/frontend/src/Auth.jsx
+++ gestion-pharmacie/frontend/src/Auth.jsx
@@ -2,17 +2,13 @@ import { useState } from "react";
 import './css/auth.css';
 
 export default function Auth() {
-  const [role, setRole] = useState('superAdmin');
-  const [superAdminData, setSuperAdminData] = useState({ password: '' });
-  const [gestionnaireStockData, setGestionnaireStockData] = useState({ email: '', password: '' });
-  const [adminBaseData, setAdminBaseData] = useState({ email: '', password: '' });
+  
+  const [formData, setFormData] = useState({ userName: '', password: '' });
 
 
 
-  async function handleSuperAdminSubmit(e) {
+  async function handleSubmit(e) {
     e.preventDefault();
-    
-    const formdata = role === 'superAdmin' ? superAdminData : role === 'gestionnaireStock' ? gestionnaireStockData : adminBaseData;
 
     try{
         const response = await fetch('http://localhost:3000/api/auth/login', {
@@ -20,7 +16,7 @@ export default function Auth() {
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(formdata)
+            body: JSON.stringify(formData)
         })
     }
     catch(err){
@@ -28,91 +24,7 @@ export default function Auth() {
     }
   }
 
-  const renderForm = () => {
-    switch (role) {
-      case 'superAdmin':
-        return superAdminForm();
-      case 'gestionnaireStock':
-        return gestionnaireStockForm();
-      case 'adminBase':
-        return adminBaseForm();
-      default:
-        return null;
-    }
-  };
-
-  const superAdminForm = () => {
-    return (
-      <form onSubmit={handleSuperAdminSubmit} className="auth-form">
-        <h3>Connexion Super Admin</h3>
-        <div className="input-group">
-          <input
-            onChange={(e) => setSuperAdminData({ ...superAdminData, password: e.target.value })}
-            type="password"
-            required
-            placeholder="Entrez la clé secrète.."
-          />
-        </div>
-        <button type="submit" className="submit-btn">
-          Se Connecter
-        </button>
-      </form>
-    );
-  };
-
-  const gestionnaireStockForm = () => {
-    return (
-      <form onSubmit={handleSuperAdminSubmit} className="auth-form">
-        <h3>Connexion Gestionnaire de Stock</h3>
-        <div className="input-group">
-          <input
-            onChange={(e) => setGestionnaireStockData({ ...gestionnaireStockData, email: e.target.value })}
-            type="email"
-            required
-            placeholder="Entrez Votre Email.."
-          />
-        </div>
-        <div className="input-group">
-          <input
-            onChange={(e) => setGestionnaireStockData({ ...gestionnaireStockData, password: e.target.value })}
-            type="password"
-            required
-            placeholder="Entrez la clé secrète.."
-          />
-        </div>
-        <button type="submit" className="submit-btn">
-          Se Connecter
-        </button>
-      </form>
-    );
-  };
-
-  const adminBaseForm = () => {
-    return (
-      <form onSubmit={handleSuperAdminSubmit} className="auth-form">
-        <h3>Connexion Admin de Base</h3>
-        <div className="input-group">
-          <input
-            onChange={(e) => setAdminBaseData({ ...adminBaseData, email: e.target.value })}
-            type="email"
-            required
-            placeholder="Entrez Votre Email.."
-          />
-        </div>
-        <div className="input-group">
-          <input
-            onChange={(e) => setAdminBaseData({ ...adminBaseData, password: e.target.value })}
-            type="password"
-            required
-            placeholder="Entrez la clé secrète.."
-          />
-        </div>
-        <button type="submit" className="submit-btn">
-          Se Connecter
-        </button>
-      </form>
-    );
-  };
+  
 
   return (
     <div className="auth-container">
@@ -129,29 +41,29 @@ export default function Auth() {
       <div className="auth-content">
         <h1 className="main-title">Plateforme de gestion de la Pharmacie centrale</h1>
         
-        <div className="role-nav">
-          <button 
-            className={`role-btn ${role === 'superAdmin' ? 'active' : ''}`}
-            onClick={() => setRole('superAdmin')}
-          >
-            Super-Admin
-          </button>
-          <button 
-            className={`role-btn ${role === 'gestionnaireStock' ? 'active' : ''}`}
-            onClick={() => setRole('gestionnaireStock')}
-          >
-            Gestionnaire-Stock
-          </button>
-          <button 
-            className={`role-btn ${role === 'adminBase' ? 'active' : ''}`}
-            onClick={() => setRole('adminBase')}
-          >
-            Admin-Base
-          </button>
-        </div>
-        
         <div className="form-container">
-          {renderForm()}
+        <form onSubmit={handleSubmit} className="auth-form">
+        <h3>Connexion</h3>
+        <div className="input-group">
+          <input
+            onChange={(e) => setFormData({ ...formData, userName: e.target.value })}
+            type="text"
+            required
+            placeholder="Entrez Votre UserName.."
+          />
+        </div>
+        <div className="input-group">
+          <input
+            onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+            type="password"
+            required
+            placeholder="Entrez la clé secrète.."
+          />
+        </div>
+        <button type="submit" className="submit-btn">
+          Se Connecter
+        </button>
+      </form>
         </div>
       </div>
     </div>
