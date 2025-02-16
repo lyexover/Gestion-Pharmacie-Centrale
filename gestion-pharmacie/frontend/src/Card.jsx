f1849c3c9a79d5955dbffaf7422d74ec310dbb7c
lyes <lyesindel4@gmail.com>
2025-02-16 17:31:08 +0100

    updated produits

--- gestion-pharmacie/frontend/src/Card.jsx
+++ gestion-pharmacie/frontend/src/Card.jsx
@@ -1,11 +1,20 @@
+import { Link } from "react-router-dom"
+
+
 export default function Card({data, type}){
 
 
     return (
         <div className="stock-card">
             <div className="stock-card-header">
-                <h3 className="stock-card-title">{data.nom}</h3>
-                <p className="stock-card-reference">Reference: {data.code_produit}</p>
+                <div>
+                  <h3 className="stock-card-title">{data.nom}</h3>
+                  <p className="stock-card-reference">Reference: {data.code_produit}</p>
+                </div>
+                <div className="right">
+                    <button className="delete-btn"><i class="fa-solid fa-trash"></i></button>
+                    <Link className='edit-btn'><i class="fa-solid fa-pen-to-square"></i></Link>
+                </div>
             </div>
             
             <div className="stock-card-content">
@@ -19,7 +28,7 @@ export default function Card({data, type}){
                     <div className="stock-card-lot">
     
                         <p className="stock-card-quantity">Quantite : {data.quantite_disponible}</p>
-                        <p className="stock-card-date">Date peremption : {data.date_peremption}</p>
+                        <p className="stock-card-date">Date peremption : {new Date(data.date_peremption).toLocaleDateString()}</p>
                     </div>
                 )}
             </div>
